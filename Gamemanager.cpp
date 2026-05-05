f4b05c7550600658a5cb57686ea23ee6924fb341
Hiwkowlnwza007 <kittipit9058@gmail.com>
2026-05-06 00:25:29 +0700

    วางโครงGamemanager รอplayer board class

--- Gamemanager.cpp
+++ Gamemanager.cpp
@@ -1,1 +1,17 @@
-//gamemanager+player
+#include "Gamemanager.h"
+
+void Gamemanager::setupgame(){
+    //setupcode
+}
+
+void Gamemanager::startgame(){
+    //startcode
+}
+
+void Gamemanager::wincheck(){
+    //wincode
+}
+
+void Gamemanager::winnerdeclare(){
+    //winnerdeclarecode
+}
--- Gamemanager.h
+++ Gamemanager.h
@@ -1,4 +1,17 @@
 #pragma once //do not delete this line
+#include "Player.h"
+#include <vector>
+#include "Board.h"
 
-
-//gamemanager
+class Gamemanager{
+private:
+    std::vector<Player> players;
+    Board board;
+    bool isgameover = false;
+    int nowplayerindex = 0;
+public:
+    void setupgame();
+    void startgame();
+    void wincheck();
+    void winnerdeclare(); 
+};
