d41f49749abe771086c31e87f0069ee247dcad17
NeenNin <neens2592549@gmail.com>
2026-05-16 22:10:36 +0700

    setupCards

--- Board.h
+++ Board.h
@@ -1,5 +1,6 @@
 #pragma once //do not delete this line
 #include "Card.h"
+#include "Cardgroup.h"
 #include <vector>
 #include <string>
 //board ไม่ต้องเขียนgemแล้ว คีย์เขียนละ
@@ -7,11 +8,19 @@ class Board{
     private:
         Gem board_gem;
         std::vector<NobilityCard> board_nobility;
-        std::vector<DevelopmentCard> board_development;
-        //std::vector<Deck> board_deck;
+        std::vector<DevelopmentCard> board_development_tier1;
+        std::vector<DevelopmentCard> board_development_tier2;
+        std::vector<DevelopmentCard> board_development_tier3;
+        Deck board_deck_tier2;
+        Deck board_deck_tier3;
 
     public:
-        bool gemmanagement(std::string Action, Gem g);
-        void replacecard();
-        void setupGems(int num_player);
+        bool gemmanagement(std::string Action, Gem g);//เช็คจำนวนgem บนboard
+        void replacecard(int tier, int index); //จั่วการ์ดเพิ่ม
+        void setupGems(int num_player);//ตั้งค่าgemบนboardตามจำนวนผู้เล่น
+        void setupCards(std::vector<DevelopmentCard>& tier1,//ตั้งค่าการ์ดบนboardตามจำนวนผู้เล่น 
+            std::vector<DevelopmentCard>& tier2, 
+            std::vector<DevelopmentCard>& tier3, 
+            std::vector<NobilityCard>& nobilityCards, 
+            int num_player);
 };
