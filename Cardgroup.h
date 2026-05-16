f1234b97a9f778e894c2b6057d6b22e2db71b880
mintyuparat <mintypr6462@gmail.com>
2026-05-16 22:15:56 +0700

    Cardgroup
    1.addcard เรียบร้อย
    2.removecard เรียบร้อย
    3.sizecard เรียบร้อย
    4.calculategem เรียบร้อย
    5.draw เรียบร้อย

--- Cardgroup.h
+++ Cardgroup.h
@@ -10,16 +10,23 @@ class Cardgroup {
     public :
         void addcard(DevelopmentCard addcard_);
         void removecard(DevelopmentCard removecart_);
-        int sizecard(std::vector<DevelopmentCard> sizecard_);
+        int sizecard();
 };
 
 class Playerhand : public Cardgroup {
     public :
-        Gem calculategem(std::vector<DevelopmentCard> calculategem_);
+        Gem calculategem();
 };
 
 class Deck : public Cardgroup {
+    private :
+        std::vector<DevelopmentCard> Tier1;
+        std::vector<DevelopmentCard> Tier2;
+        std::vector<DevelopmentCard> Tier3;
     public :
-        void shuffle(); //ปป ตน card devolop
-        DevelopmentCard draw();
+        void shuffle();
+        std::vector<DevelopmentCard> getTier1() const { return Tier1; }
+        std::vector<DevelopmentCard> getTier2() const  { return Tier2; }
+        std::vector<DevelopmentCard> getTier3() const  { return Tier3; } 
+        DevelopmentCard draw(std::vector<DevelopmentCard> draw_);
 };
